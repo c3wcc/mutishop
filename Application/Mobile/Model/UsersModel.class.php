2d17d9323ceed35a347e491ce41b11ef79da6b66
fuzhijingcom <yudwcom@163.com>
2019-01-24 23:11:17 +0800

    登录注册\更新信息

--- Application/Mobile/Model/UsersModel.class.php
+++ Application/Mobile/Model/UsersModel.class.php
@@ -6,7 +6,10 @@ use Think\Model;
 
 class UsersModel extends Model{
     
-    
+    /**
+     * 注册
+     * 更新头像
+     */
     public function register($data)
     {
         if(!$data['user_id']){
@@ -15,11 +18,21 @@ class UsersModel extends Model{
 
         $user = M('users')->where(["user_id"=>$data['user_id'] ])->find();
         if($user){
+            //更新信息
+            if($user['nickname'] != $data['nickname']){
+                M('users')->where(["user_id"=>$data['user_id']])->update(['nickname' => $data['nickname']]);
+            }
+
+            if($user['head_pic'] != $data['head_pic']){
+                M('users')->where(["user_id"=>$data['user_id']])->update(['head_pic' => $data['head_pic']]);
+            }
+
             return false;
         }
 
         $add['user_id'] = $data['user_id'];
         $add['openid'] = $data['openid'];
+        $add['nickname'] = $data['nickname'];
         $add['head_pic'] = $data['head_pic'];
         M('users')->data($add)->add();
 
