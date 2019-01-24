361a3f5d91646a217ce80c06cd20f2f08b65633c
fuzhijingcom <yudwcom@163.com>
2019-01-24 23:05:11 +0800

    登录注册

--- Application/Mobile/Model/UsersModel.class.php
+++ Application/Mobile/Model/UsersModel.class.php
@@ -9,16 +9,15 @@ class UsersModel extends Model{
     
     public function register($data)
     {
-        $user = self::where(["user_id"=>$data['user_id'] ])->find();
+        $user = M('users')->where(["user_id"=>$data['user_id'] ])->find();
         if($user){
             return false;
         }
 
-        
         $add['user_id'] = $data['user_id'];
         $add['openid'] = $data['openid'];
         $add['head_pic'] = $data['head_pic'];
-        self::data($add)->add();
+        M('users')->data($add)->add();
 
     }
 }
